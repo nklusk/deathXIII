d9bd5b1b1aba99514e41771e3a37f6163b402b87
NATALIE LUSK <NKL22@fsu.edu>
2023-10-30 11:01:17 -0400

    title screen

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
-function setup() {
-  createCanvas(400, 400);
+let deathSpiral;
+let angle = 0;
+
+function preload(){
+  deathTitleText = loadImage("assets/deathTitleText.png");
+  deathSpiral = loadImage("assets/deathSpiral.png");
 }
 
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(31, 63, 49);
+  imageMode(CENTER);
+  image(deathTitleText, windowWidth/2, windowHeight/3);
+  angleMode(DEGREES);
+}
+
+
+
 function draw() {
-  background(125);
+  background(canvas);
+
+  push();
+  translate(width/2, height);
+  rotate(45);
+  rotate(frameCount/10);
+  image(deathSpiral, 0, 0);
+  pop();
+
+  
 }
